7d34c2fd47106fa346754861912782c84bb379b9
mahmoo30 <wasif.96@gmail.com>
2023-06-27 16:16:39 -0600

    added User model with comments

--- models/User.js
+++ models/User.js
@@ -1,23 +1,57 @@
-// User
+const { Schema, model } = require('mongoose');
 
-// username
+// Schema to create a User model
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        unique: true,
 
-// String
-// Unique
-// Required
-// Trimmed
-// email
+        // Validator for email address
+        validate: {
+            validator: function(v) {
+                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: "Please enter a valid email"
+        },
+        required: [true, "Email required"]
+    },
+      thoughts: [{
+        // Array of _id values referencing the Thought model
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }],
+      friends: [{
+        // Array of _id values referencing the User model (self-reference)
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }],
+  },
+  {
+    toJSON: {
+      // Mongoose will not include virtuals by default, so a `virtuals` property is added and set it's value to true
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-// String
-// Required
-// Unique
-// Must match a valid email address (look into Mongoose's matching validation)
-// thoughts
+// A virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+  return `friends: ${this.friends.length}`;
+  });
 
-// Array of _id values referencing the Thought model
-// friends
+// Initialize the User model
+const User = model('User', userSchema);
 
-// Array of _id values referencing the User model (self-reference)
-// Schema Settings
-
-// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+// Export the User model
+module.exports = User;
