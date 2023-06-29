4dbbaa3658e98498f4d620bc51e1a2467600812d
mahmoo30 <wasif.96@gmail.com>
2023-06-29 17:20:59 -0600

    pre test code done

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+// Require methods for Thoughts & Reactions
+const {createThought, getThoughts, getSingleThought, updateThought, deleteThought, addReaction, deleteReaction} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts);
+router.route('/:userId').post(createThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought);
+router.route('/:thoughtId/users/:userIdno').delete(deleteThought);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+// Require methods for User & Friend
+const {createUser, getUsers, getSingleUser, updateSingleUser, deleteSingleUser, addFriend, deleteFriend} = require('../../controllers/userController.js');
+
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).put(updateSingleUser).delete(deleteSingleUser);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend)
+
+module.exports = router;
